ff351b3aac874d111a8bd7882db5b20a069e2641
Alex Huang <alexhuang91@gmail.com>
2012-10-04 14:56:17 -0700

    HW3 finished! Code grader attached

--- hw3/curves2.cpp
+++ hw3/curves2.cpp
@@ -29,6 +29,7 @@ void WorkingScene::mouse(int button, int state, int x, int y) {
     }
     if (button == GLUT_RIGHT_BUTTON) {	
       /* YOUR CODE HERE */
+      theOnlyCurve->updateActivePoint(normx,normy);
       theOnlyCurve->deleteActivePoint();
     }
   }
@@ -49,10 +50,40 @@ void WorkingScene::mouse(int button, int state, int x, int y) {
 void Bezier::draw(int levelOfDetail) {
 
   connectTheDots();
-  int i,j,k;
-  Pvector::iterator p;
+  int i,j,n, level;
+  float u,x,y;
+  Pvector *bezier = new Pvector();
+  Pvector::iterator p, bez, end;
 
-  /* YOUR CODE HERE */
+  /*YOUR CODE HERE*/
+  for (j = 0; j <= levelOfDetail; j++) {      //for loop for every level of detail
+    u = j/(float)levelOfDetail;              //"level of detail" = 0, 1/levelOfDetail, ..., 1
+    n = points.size() - 1;                   //degree, which is # of points - 1
+    Pvector pts = points;                    //there are n pts initially
+    for (level = n - 1; level >= 0; level--) {   //for each point
+      Pvector *newpts = new Pvector();       //Create a vector of points
+      p = pts.begin();                       //initialize an iterator
+      for (i = 0; i <= level; i++) {         //for every level
+        x = (1-u) * (p->x) + u * ((p+1)->x); //calculate x
+        y = (1-u) * (p->y) + u * ((p+1)->y); //calculate y
+        (*newpts).push_back(Point(x,y));     //push Point(x,y) to newpts
+        p++;                                 //increment
+      }
+      pts = *newpts;                         //assign newpts as pts
+    }
+    (*bezier).push_back(Point(x,y));         //push to bezier vector
+  }
+  int iter = 1;
+  if (points.size() > 1) {
+    for (bez = (*bezier).begin(); bez != (*bezier).end(); bez++) {
+      if(iter == 0) {
+        drawLine(bez->x,bez->y,(bez-1)->x, (bez-1)->y);
+      }
+      else {
+        iter--;
+      }
+    }
+  }
 }
 
 
@@ -62,22 +93,50 @@ void Bezier::draw(int levelOfDetail) {
 // The B-Spline drawing routine.  
 // Remember to call drawSegment (auxiliary function) for each set of 4 points.
 void Bspline::draw(int levelOfDetail) {
-
-
   connectTheDots();
   /* YOUR CODE HERE */
+  int i = 1;
+  Pvector::iterator p1, p2, p3, p4;
+  for (Pvector::iterator it = points.begin(); it != points.end(); it++) {
+    //if (i > 4) {
+    //drawLine(p1->x,p1->y,p2->x,p2->y);
+    //}
+    p1 = p2;
+    p2 = p3;
+    p3 = p4;
+    p4 = it;
+    if (i > 3) {
+      drawSegment(p1, p2, p3, p4, levelOfDetail);
+    }
+    i++;
+  }
 }
 
 void Bspline::drawSegment(Pvector::iterator p1, Pvector::iterator p2, 
     Pvector::iterator p3, Pvector::iterator p4, int levelOfDetail) {
 
-  float x, y;
+  float u, x, y, xold, yold;
+  int i;
   /* YOUR CODE HERE */
-
   //draw segment
-
+  xold = (1/6.0f)*((float)((p1->x) + 4*(p2->x) + (p3->x)));
+  yold = (1/6.0f)*((float)((p1->y) + 4*(p2->y) + (p3->y)));
+  Point(xold,yold).draw();
+  for (i = 1; i <= levelOfDetail; i++) {
+    u = i/((float)levelOfDetail);
+    x =  u*u*u*((1/6.0f)*((float)( -(p1->x) + 3*(p2->x) - 3*(p3->x) + (p4->x))))
+        +  u*u*((1/6.0f)*((float)(3*(p1->x) - 6*(p2->x) + 3*(p3->x))))
+        +    u*((1/6.0f)*((float)(-3*(p1->x) + 3*(p3->x))))
+        +      ((1/6.0f)*((float)(  (p1->x) + 4*(p2->x) +   (p3->x))));
+    y =  u*u*u*((1/6.0f)*((float)( -(p1->y) + 3*(p2->y) - 3*(p3->y) + (p4->y))))
+        +  u*u*((1/6.0f)*((float)(3*(p1->y) - 6*(p2->y) + 3*(p3->y))))
+        +    u*((1/6.0f)*((float)(-3*(p1->y) + 3*(p3->y))))
+        +      ((1/6.0f)*((float)(  (p1->y) + 4*(p2->y) +   (p3->y))));
+    drawLine(xold,yold,x,y);
+    xold = x;
+    yold = y;
+  }
   //then create a Point to be drawn where the knot should be
-
   Point p(x, y);
   p.draw();
 }
@@ -95,6 +154,7 @@ void accumulateNextLevel(Pvector* accum, Pvector pts) {
   if (pts.size() == 1) return;
   for (Pvector::iterator it = pts.begin(); it != pts.end() - 1; it++) {
     /* YOUR CODE HERE  (only one to three lines)*/
+    *it = Point(((1/2.0f)*((it->x) + ((it+1)->x))),((1/2.0f)*((it->y) + ((it+1)->y))));
   }
   //save the last point
   Point last = *(pts.end()-1);
@@ -121,9 +181,8 @@ void Bezier2::draw(int levelOfDetail) {
   if (levelOfDetail <= 1) {
     if (points.size() >= 2) {
       for (Pvector::iterator it = points.begin(); it != points.end() - 1; it++) {
-
         /* YOUR CODE HERE */
-
+        drawLine((it->x),(it->y),((it+1)->x),((it+1)->y));
       }
     }
   } else {
@@ -133,7 +192,19 @@ void Bezier2::draw(int levelOfDetail) {
     //add the correct points to 'left' and 'right'.
     //You may or may not use accum as you see fit.
     /* YOUR CODE HERE */
-
+    accumulateNextLevel(accum, points);
+    Pvector::iterator it = (*accum).begin();
+    int size = (*accum).size();
+    for (int i = 1; i <= size; i++) {
+      int half = size/2;
+      if ((i <= half) || (((size % 2) == 1) && (i == (half + 1)))) {
+        left.points.push_back(*it);
+      }
+      if (i > half) {
+        right.points.push_back(*it);
+      }
+      it++;
+    }
     left.draw(1-levelOfDetail);
     right.draw(1-levelOfDetail);
     delete accum;
